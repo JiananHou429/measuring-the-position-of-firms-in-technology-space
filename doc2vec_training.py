16a327de6b8bb48afb3f01fae076e70c1636738a
nicolamelluso <64489767+nicolamelluso@users.noreply.github.com>
2023-07-03 20:51:12 +0200

    Added pre-processing

--- doc2vec_training.py
+++ doc2vec_training.py
@@ -1,17 +1,60 @@
 """
 --------------------------------------------------------------------------- 
-Script to train a Doc2Vec model for patents and get the vectors from the trained model.
-"Measuring the Position and Differentiation of Firms in
- Technology Space" -- Sam Arts, Bruno Cassiman, and Jianan Hou
+Script to (1) pre-process the patent text, (2) train a Doc2Vec model for patents and (3) get the vectors from the trained model.
+Suppose having all the data stored (input and output) in the path './data'.
+
+"Measuring the Position and Differentiation of Firms in Technology Space" -- Sam Arts, Bruno Cassiman, and Jianan Hou
 ---------------------------------------------------------------------------
 """
 
-# Import necessary libraries
 import pandas as pd
 import gensim
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
-from gensim.utils import simple_preprocess
-from sklearn.metrics.pairwise import cosine_similarity
+from nltk.stem import PorterStemmer
+
+########## Preprocess the patent text ##########
+
+# Define the path for the raw patent text and keywords
+## 'patent txt raw' contains patents sorted by patent id with four columns with the patent id, title, abstract and claims
+## 'keywords' contains patents sorted by patent id with two columns with the patent id and the number of unique stemmed keywords
+path_patents = './data/patent txt raw.csv'
+path_keywords = './data/keywords.csv'
+
+# Define the text pre-processed data path
+processed_data_path = './data/patents_processed.csv'
+
+# Create or overwrite the existing file with only headers
+with open(processed_data_path,'w') as writer:
+    writer.write('patent,text\n')
+
+# Create a Porter Stemmer instance
+stemmer = PorterStemmer()
+
+# Load the raw patent data and the keywords data in chunks to handle large datasets
+ds_raw = pd.read_csv(path_patents, on_bad_lines = 'skip', chunksize = 1000, converters = {'patent':int})
+ds_keywords = pd.read_csv(path_keywords, on_bad_lines = 'skip', chunksize = 1000, converters = {'patent':int})
+
+# Process each chunk of data. Given that the patents are ordered by patent id, same chunks contains the same patents
+# Process by chunk in order to do not use large memory
+for df_raw, df_keywords in zip(ds_raw, ds_keywords):
+    
+    # Merge the raw and keywords dataframes on the 'patent' column
+    df = pd.merge(df_raw, df_keywords, on=['patent']).fillna('')
+
+    # Combine 'title', 'abstract' and 'claims' to form the 'text' column
+    df['text'] = df['title'] + df['abstract'] + df['claims']
+
+    # Apply stemming to the 'text' column
+    df['text'] = df['text'].apply(lambda x: ' '.join([stemmer.stem(t) for t in x.split(' ')]))
+    
+    # Keep only the words in 'text' that are also in 'keywords'
+    df['text'] = df.apply(lambda x: ' '.join([t for t in x['text'].split(' ') if t in x['keywords'].split(' ')]), axis=1)
+
+    # Write the processed chunk to the csv file
+    df[['patent', 'text']].to_csv(processed_data_path, header=False, index=False, mode='a')
+
+
+########## Train the Doc2vec ##########
 
 # Read the data from the csv file with all patent texts
 df = pd.read_csv('./data/all_patent_text.csv.csv')
@@ -29,7 +72,11 @@ model.build_vocab(documents)
 model.train(documents, total_examples=model.corpus_count, epochs=10)
 
 # Save the trained model for later use
-model.save("patent_doc2vec.model")
+model.save("./data/doc2vec.model")
+
+
+########## Get the patent vectors ##########
+
 
 # Create a new DataFrame to store the patent vectors
 df_vectors = pd.DataFrame()
